43d71aaabcb01d673c4c1ebc1a7f602157aec41b
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2025-05-23 09:16:30 -0600

    Modelo usuario

---
+++ GameStore/GameStore/Models/GameStoreUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace GameStore.Models
+{
+    public class GameStoreUser : IdentityUser
+    {
+
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public DateTime? FechaModificacion { get; set; }
+
+    }
+}
